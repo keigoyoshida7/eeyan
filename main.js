2461b03574d434f98931b1abfdb6d1f5b472d611
keigoyoshida7 <83452421+keigoyoshida7@users.noreply.github.com>
2023-05-11 15:32:50 +0900

    first

---
+++ main.js
@@ -0,0 +1,27 @@
+const button = document.getElementById("play-button");
+
+button.addEventListener("click", () => {
+  const audio = new Audio("uedasan.wav");
+  audio.play();
+});
+
+const button2 = document.getElementById("play-button2");
+
+button.addEventListener("click", () => {
+  const audio = new Audio("satasan.wav");
+  audio.play();
+});
+
+const button3 = document.getElementById("play-button3");
+
+button.addEventListener("click", () => {
+  const audio = new Audio("fukatsusan.wav");
+  audio.play();
+});
+
+const button4 = document.getElementById("play-button4");
+
+button.addEventListener("click", () => {
+  const audio = new Audio("keigo.wav");
+  audio.play();
+});
